bf7c0f374136324c7d88dd81db487b4fdebbc6be
4EverStudent <mail@4everstudent.com>
2016-10-21 20:32:07 +0100

    Add code to zoom in. Add Api Key.

--- app/src/main/java/a4everstudent/mapsdemo/MapsActivity.java
+++ app/src/main/java/a4everstudent/mapsdemo/MapsActivity.java
@@ -42,5 +42,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
