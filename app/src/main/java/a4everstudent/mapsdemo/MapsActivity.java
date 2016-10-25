c2c058bae8f628d03cdf3e9cb46e3c80634f6f4a
4EverStudent <mail@4everstudent.com>
2016-10-25 19:26:43 +0100

    Add code to test markers options. (label, color, map_type)

--- app/src/main/java/a4everstudent/mapsdemo/MapsActivity.java
+++ app/src/main/java/a4everstudent/mapsdemo/MapsActivity.java
@@ -1,12 +1,13 @@
 package a4everstudent.mapsdemo;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,22 +27,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     *
+     // Add a marker in Sydney and move the camera
+     LatLng sydney = new LatLng(-34, 151);
+     mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+     //Zooom in
+     mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+     // new place
+     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(42.813822, 23.726292), 15));
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        //change map type to terrain
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        //change marker position and label to Taj Mahal, also changed color of the marker
+        mMap.addMarker(new MarkerOptions().position(new LatLng(27.175010, 78.042144)).title("Taj Mahal").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        //move camera to there and zoom it
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(27.175010, 78.042144),15));
+
+
     }
 }
